6045c9fe5b2dd55506714d001100ac8b9d810e16
ishikashrivastava-05 <shrivastavaishika373@gmail.com>
2023-07-07 14:07:34 +0530

    javascri[pt files

---
+++ 7thjuly.js
@@ -0,0 +1,9 @@
+//  let x = function user(){
+//     console.log('this is user1:')
+// }
+// x();
+function user(name){
+    console.log(`this is user1: ${name}`)
+}
+user("ravi");
+
--- prct.js
+++ prct.js
@@ -15,12 +15,37 @@
 // }
 // login('ishika');
 
-function sum ( )
+// function sum ( )
 
 
 
-function uppercase (str){
-    return str.toUppercase();
-}
-const result = uppercase ('javascript');
-console.log(result)
+// function uppercase (str){
+//     return str.toUppercase();
+// }
+// const result = uppercase ('javascript');
+// console.log(result)
+
+
+// function prati(){
+//     console.log("hello");
+// }
+// prati();
+
+
+// 
+// var add = function(){
+//     console.log("a+b")
+// }
+// add(5,5);
+
+// var add=()=>{
+//     console.log("jg");
+// }
+// add();
+// console.log(a);
+// const a=10;
+// add();
+// function add (){
+//     console.log("hii"
+//     )
+// }
